a88ec7c37410b235e32cbb0209ec1d9341b5329a
mboras <borasmislav@gmail.com>
2014-04-13 23:31:39 +0200

    Inicijalni solution

--- projekt/app/DriveIT/DriveIT/Login.Designer.cs
+++ projekt/app/DriveIT/DriveIT/Login.Designer.cs
@@ -48,6 +48,7 @@
             this.ClientSize = new System.Drawing.Size(840, 525);
             this.Controls.Add(this.btnLogin);
             this.Name = "frmDriveIT";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DriveIT";
             this.ResumeLayout(false);
 
