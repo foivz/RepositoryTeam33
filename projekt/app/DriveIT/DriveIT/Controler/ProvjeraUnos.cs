2b8cfa40f9963c8245ca5759db878073a3480659
jprimorac <jprimorac92@gmail.com>
2014-06-14 12:07:00 +0200

    Provjera 2

---
+++ projekt/app/DriveIT/DriveIT/Controler/ProvjeraUnos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveIT.Controler
+{
+    class ProvjeraUnos
+    {
+        public static bool ProvjeraIme(string unos)
+        {
+            return true;
+        }
+
+    }
+}
