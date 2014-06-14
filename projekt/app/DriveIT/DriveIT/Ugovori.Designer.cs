d4c7402e4e1d3272130eabf9005d5b0eabb4a9e5
maleric <markoall1@gmail.com>
2014-06-14 21:35:23 +0200

    Ugovori dodani

--- projekt/app/DriveIT/DriveIT/Ugovori.Designer.cs
+++ projekt/app/DriveIT/DriveIT/Ugovori.Designer.cs
@@ -28,9 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnIzlaz = new MetroFramework.Controls.MetroButton();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ugovorBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.idugovorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kupacDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.voziloDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dobavljaciddobavljacDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dobavljacDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.kupac1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.vozilo1DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ugovorBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // btnIzlaz
@@ -48,11 +59,74 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idugovorDataGridViewTextBoxColumn,
+            this.kupacDataGridViewTextBoxColumn,
+            this.voziloDataGridViewTextBoxColumn,
+            this.datumDataGridViewTextBoxColumn,
+            this.dobavljaciddobavljacDataGridViewTextBoxColumn,
+            this.dobavljacDataGridViewTextBoxColumn,
+            this.kupac1DataGridViewTextBoxColumn,
+            this.vozilo1DataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.ugovorBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(30, 83);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(487, 276);
             this.dataGridView1.TabIndex = 1;
+            // 
+            // ugovorBindingSource
+            // 
+            this.ugovorBindingSource.DataSource = typeof(DriveIT.Database.ugovor);
+            // 
+            // idugovorDataGridViewTextBoxColumn
+            // 
+            this.idugovorDataGridViewTextBoxColumn.DataPropertyName = "id_ugovor";
+            this.idugovorDataGridViewTextBoxColumn.HeaderText = "id_ugovor";
+            this.idugovorDataGridViewTextBoxColumn.Name = "idugovorDataGridViewTextBoxColumn";
+            // 
+            // kupacDataGridViewTextBoxColumn
+            // 
+            this.kupacDataGridViewTextBoxColumn.DataPropertyName = "kupac";
+            this.kupacDataGridViewTextBoxColumn.HeaderText = "kupac";
+            this.kupacDataGridViewTextBoxColumn.Name = "kupacDataGridViewTextBoxColumn";
+            // 
+            // voziloDataGridViewTextBoxColumn
+            // 
+            this.voziloDataGridViewTextBoxColumn.DataPropertyName = "vozilo";
+            this.voziloDataGridViewTextBoxColumn.HeaderText = "vozilo";
+            this.voziloDataGridViewTextBoxColumn.Name = "voziloDataGridViewTextBoxColumn";
+            // 
+            // datumDataGridViewTextBoxColumn
+            // 
+            this.datumDataGridViewTextBoxColumn.DataPropertyName = "datum";
+            this.datumDataGridViewTextBoxColumn.HeaderText = "datum";
+            this.datumDataGridViewTextBoxColumn.Name = "datumDataGridViewTextBoxColumn";
+            // 
+            // dobavljaciddobavljacDataGridViewTextBoxColumn
+            // 
+            this.dobavljaciddobavljacDataGridViewTextBoxColumn.DataPropertyName = "dobavljac_iddobavljac";
+            this.dobavljaciddobavljacDataGridViewTextBoxColumn.HeaderText = "dobavljac_iddobavljac";
+            this.dobavljaciddobavljacDataGridViewTextBoxColumn.Name = "dobavljaciddobavljacDataGridViewTextBoxColumn";
+            // 
+            // dobavljacDataGridViewTextBoxColumn
+            // 
+            this.dobavljacDataGridViewTextBoxColumn.DataPropertyName = "dobavljac";
+            this.dobavljacDataGridViewTextBoxColumn.HeaderText = "dobavljac";
+            this.dobavljacDataGridViewTextBoxColumn.Name = "dobavljacDataGridViewTextBoxColumn";
+            // 
+            // kupac1DataGridViewTextBoxColumn
+            // 
+            this.kupac1DataGridViewTextBoxColumn.DataPropertyName = "kupac1";
+            this.kupac1DataGridViewTextBoxColumn.HeaderText = "kupac1";
+            this.kupac1DataGridViewTextBoxColumn.Name = "kupac1DataGridViewTextBoxColumn";
+            // 
+            // vozilo1DataGridViewTextBoxColumn
+            // 
+            this.vozilo1DataGridViewTextBoxColumn.DataPropertyName = "vozilo1";
+            this.vozilo1DataGridViewTextBoxColumn.HeaderText = "vozilo1";
+            this.vozilo1DataGridViewTextBoxColumn.Name = "vozilo1DataGridViewTextBoxColumn";
             // 
             // frmUgovori
             // 
@@ -64,7 +138,9 @@
             this.Name = "frmUgovori";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Ugovori";
+            this.Load += new System.EventHandler(this.frmUgovori_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ugovorBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -73,5 +149,14 @@
 
         private MetroFramework.Controls.MetroButton btnIzlaz;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idugovorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kupacDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn voziloDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datumDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dobavljaciddobavljacDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dobavljacDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn kupac1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vozilo1DataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource ugovorBindingSource;
     }
 }
--- projekt/app/DriveIT/DriveIT/Ugovori.cs
+++ projekt/app/DriveIT/DriveIT/Ugovori.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DriveIT.Database;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,13 @@ namespace DriveIT
 {
     public partial class frmUgovori : Form
     {
+        private void PrikaziUgovore()
+        {
+            T33_DBEntities db = new T33_DBEntities();
+            var ugovori = db.ugovor.ToList<ugovor>();
+            ugovorBindingSource.DataSource = ugovori;
+        }
+
         public frmUgovori()
         {
             InitializeComponent();
@@ -20,6 +28,11 @@ namespace DriveIT
         private void btnIzlaz_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void frmUgovori_Load(object sender, EventArgs e)
+        {
+            PrikaziUgovore();
         }
     }
 }
