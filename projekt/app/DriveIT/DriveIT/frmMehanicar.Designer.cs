a7d450d22f5db58a4b70ee54fc553ffc1a72c11b
maleric <markoall1@gmail.com>
2014-06-21 14:11:03 +0200

    Mehanicar: notifikacije za zadnje paljenje

--- projekt/app/DriveIT/DriveIT/frmMehanicar.Designer.cs
+++ projekt/app/DriveIT/DriveIT/frmMehanicar.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.notifikacijeGrid = new System.Windows.Forms.DataGridView();
+            this.saveBtn = new MetroFramework.Controls.MetroButton();
+            this.lblDugoPaljenaVozila = new MetroFramework.Controls.MetroLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.notifikacijeGrid)).BeginInit();
             this.SuspendLayout();
+            // 
+            // notifikacijeGrid
+            // 
+            this.notifikacijeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.notifikacijeGrid.Location = new System.Drawing.Point(404, 145);
+            this.notifikacijeGrid.Name = "notifikacijeGrid";
+            this.notifikacijeGrid.Size = new System.Drawing.Size(552, 210);
+            this.notifikacijeGrid.TabIndex = 0;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Highlight = false;
+            this.saveBtn.Location = new System.Drawing.Point(881, 79);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.Style = MetroFramework.MetroColorStyle.Blue;
+            this.saveBtn.StyleManager = null;
+            this.saveBtn.TabIndex = 1;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // lblDugoPaljenaVozila
+            // 
+            this.lblDugoPaljenaVozila.AutoSize = true;
+            this.lblDugoPaljenaVozila.CustomBackground = true;
+            this.lblDugoPaljenaVozila.CustomForeColor = false;
+            this.lblDugoPaljenaVozila.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblDugoPaljenaVozila.FontWeight = MetroFramework.MetroLabelWeight.Light;
+            this.lblDugoPaljenaVozila.LabelMode = MetroFramework.Controls.MetroLabelMode.Default;
+            this.lblDugoPaljenaVozila.Location = new System.Drawing.Point(404, 115);
+            this.lblDugoPaljenaVozila.Name = "lblDugoPaljenaVozila";
+            this.lblDugoPaljenaVozila.Size = new System.Drawing.Size(235, 25);
+            this.lblDugoPaljenaVozila.Style = MetroFramework.MetroColorStyle.Blue;
+            this.lblDugoPaljenaVozila.StyleManager = null;
+            this.lblDugoPaljenaVozila.TabIndex = 3;
+            this.lblDugoPaljenaVozila.Text = "Vozila koja dugo nisu paljena";
+            this.lblDugoPaljenaVozila.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.lblDugoPaljenaVozila.UseStyleColors = false;
             // 
             // frmMehanicar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(984, 696);
+            this.ClientSize = new System.Drawing.Size(968, 462);
+            this.Controls.Add(this.lblDugoPaljenaVozila);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.notifikacijeGrid);
             this.Name = "frmMehanicar";
             this.Text = "frmMehanicar";
             this.Load += new System.EventHandler(this.frmMehanicar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.notifikacijeGrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView notifikacijeGrid;
+        private MetroFramework.Controls.MetroButton saveBtn;
+        private MetroFramework.Controls.MetroLabel lblDugoPaljenaVozila;
     }
 }
--- projekt/app/DriveIT/DriveIT/frmMehanicar.cs
+++ projekt/app/DriveIT/DriveIT/frmMehanicar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DriveIT.Database;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,11 +8,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Entity;
 
 namespace DriveIT
 {
     public partial class frmMehanicar : Form
     {
+        private BindingSource bs = new BindingSource();
+        T33_DBEntities db;
         public frmMehanicar()
         {
             InitializeComponent();
@@ -19,7 +23,50 @@ namespace DriveIT
 
         private void frmMehanicar_Load(object sender, EventArgs e)
         {
-            
+             db = new T33_DBEntities();
+             DateTime vrijeme = DateTime.Now;
+             int mj = 5; /* MJESECI */
+             vrijeme = vrijeme.AddMonths(mj*-1);
+
+             //var vozila = db.vozilo.Where<vozilo>(x => x.zadnje_paljenje == null).ToList<vozilo>();
+             /*notifikacijeGrid.DataSource = vozila;*/
+
+            db.vozilo.Where<vozilo>(x => x.zadnje_paljenje <= vrijeme).Load();
+            if (db.vozilo.Where<vozilo>(x => x.zadnje_paljenje <= vrijeme).Count() > 0)
+            {
+                bs.DataSource = db.vozilo.Local.ToBindingList();
+                notifikacijeGrid.DataSource = bs;
+
+
+                notifikacijeGrid.Columns["id_vozilo"].Visible = false;
+                notifikacijeGrid.Columns["sjedista"].Visible = false;
+                notifikacijeGrid.Columns["nosivost"].Visible = false;
+                notifikacijeGrid.Columns["prometna"].Visible = false;
+                notifikacijeGrid.Columns["tip_vozila"].Visible = false;
+                notifikacijeGrid.Columns["tip_vozila1"].Visible = false;
+                notifikacijeGrid.Columns["model_vozila"].Visible = false;
+                notifikacijeGrid.Columns["model_vozila1"].Visible = false;
+                notifikacijeGrid.Columns["parking"].Visible = false;
+                notifikacijeGrid.Columns["parking1"].Visible = false;
+                notifikacijeGrid.Columns["cijena"].Visible = false;
+            }
+            else
+            {
+                //notifikacijeGrid.Visible = false;
+                notifikacijeGrid.AllowUserToAddRows = true;
+                notifikacijeGrid.Columns.Add("status", "Status");
+                //notifikacijeGrid.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+                notifikacijeGrid.Columns[0].Width = (int) (notifikacijeGrid.Size.Width*0.92);
+                notifikacijeGrid.Rows[0].Cells[0].Value = "Ne postoji vozilo koje nije paljeno duže od "+mj+" mj";
+
+            }
+
+             
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            db.SaveChanges();
         }
     }
 }
